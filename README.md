653aebd0266be2d155b6f686de40bad13458f48d
jstormes <jstormes@stormes.net>
2014-11-02 13:23:02 -0600

    Code and README.md

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ DynamicHosts
 ============
 
 Automatic hosts file for OS X
+-----------------------------
+
+Setting up new entries in /etc/hosts is a pain.  Apache has mod_vhost_alias, but without some type of wildcard or dynamic DNS support, you still have to setup entries in hosts.
+
+With a little help from Apple's [launched](https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man8/launchd.8.html) this script can automate your /etc/hosts file.
+
+#### Quick Start
+
+Step 0:
+Backup your original hosts file.
+
+	`sudo cp -p /etc/hosts /etc/hosts.org`
+
+Step 1:
+Edit the file DynamicHosts.plist and change the two string entries that have "/usr/local/zend/apache2/htdocs/" to the path of your webroot.
+
+Step 2:
+Copy DynamicHosts.plist to /System/Library/LaunchAgents/
+
+	`sudo cp DynamicHosts.plist /System/Library/LaunchAgents/`
+
+Step 3:
+Start the monitoring:
+
+	`sudo launchctl load /System/Library/LaunchAgents/DynamicHosts.plist`
+	
+Test:
+In your webroot create a new directory.  Inside that directory create a directory.
+
+If you cat out your /etc/hosts your new direcotry/sub-directory should appear at the bottom of your hosts file as 127.0.0.1 sub-direcotry.directory
---
+++ dynamic_hosts.php
@@ -0,0 +1,145 @@
+#!/usr/bin/php
+<?php
+
+/**
+ * Class to convert {$path}/directories/subdirectories into
+ * subdmoains.domain.  Also checks that for new files less than
+ * $dirtyTime old.
+ * 
+ * @author jstormes
+ *
+ */
+class ZendDomains {
+    
+    /** 
+     * Dirty flag set if any subdirectory is only $dirtyTime old
+     * 
+     * @var unknown
+     */
+    public $dirty = false;
+    
+    /**
+     * Array of new domains
+     * 
+     * @var unknown
+     */
+    public $domains = array();
+    
+    function __construct($path = '/usr/local/zend/apache2/htdocs/', $dirtyTime = 30) {
+        // Get the domain directories
+        $domains = array_filter(glob($path.'*'), 'is_dir');
+        
+        // Get the new domains into an array
+        foreach($domains as $domain) {
+            $subDomains = array_filter(glob($path.basename($domain).'/*'), 'is_dir');
+            foreach($subDomains as $subDomain) {
+                // if we have files that have been created in the last 30 seconds
+                // we have a "dirty" hosts file so set the flag to update the hosts file.
+                if ((time()-filectime($subDomain))<$dirtyTime) $this->dirty=true;
+                $this->domains[] = basename($subDomain).".".basename($domain);
+            }
+        }
+    }
+}
+
+/**
+ * Class to manage the /etc/hosts file
+ * 
+ * @author jstormes
+ *
+ */
+class hosts {
+    
+    /**
+     * Host file location usualy /etc/hosts
+     * 
+     * @var unknown
+     */
+    private $hostFile = '';
+    
+    /**
+     * Array of hosts file lines
+     * 
+     * @var unknown
+     */
+    private $lines = array();
+    
+    /**
+     * Marker inside the hosts file to start new hosts
+     * 
+     * @var unknown
+     */
+    private $newHostsMarker = "# Begin dynamic DNS\n";
+    
+    function __construct($hostFile = '/etc/hosts') {
+        $this->hostFile = $hostFile;
+    }
+    
+    /**
+     * Get the existing hosts file lines, ignore any lines
+     * after our marker.
+     */
+    function getExistingHosts(){
+        $handle = fopen($this->hostFile, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                if ($line==$this->newHostsMarker)
+                    break;
+                $this->lines[]=$line;
+            }
+        }
+        fclose($handle);
+    }
+    
+    /**
+     * Add new hosts to our lines from the passed array.
+     * 
+     * @param unknown $domains
+     */
+    function addNewHosts($domains) {
+        // Make the first new line our marker
+        $this->lines[] = $this->newHostsMarker;
+        
+        // Append our domains into the lines
+        foreach($domains as $domain) {
+            $this->lines[] = "127.0.0.1\t{$domain}\n";
+        }
+    }
+    
+    /**
+     * Save our lines back into the hostFile.
+     */
+    function saveNewHosts() {
+        $handle = fopen($this->hostFile,"w");
+        if ($handle) {
+            foreach($this->lines as $line) {
+                fwrite($handle,$line);
+            }
+        }
+        fclose($handle);
+    }
+}
+
+
+/*
+ * Begin Entry point
+ */
+echo "argv cound ".count($argv);
+if (count($argv)==2)
+    $ZendDomains = new ZendDomains($argv[1]);
+else
+    //$ZendDomains = new ZendDomains('/usr/local/zend/apache2/htdocs/');
+    $ZendDomains = new ZendDomains('/var/www/');
+
+
+// If we have directories that are less than 30 seconds old update
+// the hosts file.
+if ($ZendDomains->dirty) {
+
+    $hosts = new hosts('/etc/hosts');
+    
+    $hosts->getExistingHosts();
+    $hosts->addNewHosts($ZendDomains->domains);
+    $hosts->saveNewHosts();
+}
+
